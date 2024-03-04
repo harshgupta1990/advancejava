654a83989342b6d94700d035058662dddff0503b
Harshit Gupta <harshgupta1990@gmil.com>
2024-03-04 19:10:10 +0530

    springboot-thymeleaf-view commit

---
+++ springboot-thymeleaf-view/src/test/java/com/wap2teach/SpringbootThymeleafViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wap2teach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
